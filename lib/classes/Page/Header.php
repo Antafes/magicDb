481c961403b1ae995ecc872b37e1ff67d998521d
friend8 <map@wafriv.de>
2014-06-27 22:04:52 +0200

    update jquery and jquery-ui to the latest releases

--- lib/classes/Page/Header.php
+++ lib/classes/Page/Header.php
@@ -22,11 +22,11 @@ class Header extends \Page
 		$this->template->loadCss('header');
 		$this->template->loadCss('common');
 		$this->template->loadCss('main');
-		$this->template->loadCss('jquery-ui-1.10.3.custom');
+		$this->template->loadCss('jquery-ui-1.11.0.custom');
 
 		// Add JS files
-		$this->template->loadJs('jquery-2.0.3');
-		$this->template->loadJs('jquery-ui-1.10.3.custom');
+		$this->template->loadJs('jquery-2.1.1');
+		$this->template->loadJs('jquery-ui-1.11.0.custom');
 		$this->template->loadJs('header');
 	}
 }
